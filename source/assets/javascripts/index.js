c33526d49b3f1da7473e6286296c237126b83044
Brad Parker <hi@bradparker.com>
2015-10-22 16:30:56 +1000

    Removes team select example

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -4,8 +4,6 @@ import React from 'react'
 import GSCLeaderMap from './components/GSCLeaderMap'
 import Leaderboards from './components/Leaderboards'
 
-import teamResults from '../data/teams.json'
-
 let Example = React.createClass({
   getInitialState () {
     return {
@@ -37,20 +35,6 @@ let Example = React.createClass({
           <GSCLeaderMap
             onTeamSelection={ this.handleTeamSelection }
             selectedTeam={ this.state.selectedTeam } />
-
-          <div className="leader-list-selector">
-            <select
-              onChange={ this.handleChange }
-              value={ this.state.selectedTeam }>
-              { teamResults.results.map((result) => {
-                  return (
-                    <option key={ result.team.id } value={ result.team.id }>
-                      { result.team.name }
-                    </option>
-                  )
-                }) }
-            </select>
-          </div>
         </div>
         <div className="panelWrap">
           <div className="panel">
