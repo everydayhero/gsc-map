98a48e8e1f3f16a29294a20aa6af11fb6d6131be
Brad Parker <hi@bradparker.com>
2015-10-30 10:46:35 +1000

    Copy change for empty state CTA

--- source/assets/javascripts/components/Leaderboards/EmptyState/index.js
+++ source/assets/javascripts/components/Leaderboards/EmptyState/index.js
@@ -13,7 +13,7 @@ export default React.createClass({
           Official ride commences Nov 1st 2015
         </h2>
         <div className="underConstruction__cta">
-          <Button kind="cta" href="https://greatsoutherncrossing.everydayhero.com/au/get-started">Sign Up</Button>
+          <Button kind="cta" href="https://greatsoutherncrossing.everydayhero.com/au/get-started">Register for free</Button>
         </div>
       </div>
     )
